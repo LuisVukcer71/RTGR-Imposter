8453dd7971ad044e3846417591ae0ece6a050f34
Luis Meinhardt <luis.meinhardt.wien@gmail.com>
2026-08-02 16:31:34 +0200

    Added p in app vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p>Luis war hier</p>
 </template>
 
 <style scoped></style>
